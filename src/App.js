cb229e64a603c34d1717cacd99c3b59f9b0f6237
Junchen Pan <junchenp1018@gmail.com>
2019-07-10 18:54:57 +0800

    some fix

--- src/App.js
+++ src/App.js
@@ -25,8 +25,8 @@ class App extends Component {
       return (
         <div
           style={{
-            position: absolute,
-            margin: auto,
+            position: "absolute",
+            margin: "auto",
             top: 0,
             right: 0,
             bottom: 0,
